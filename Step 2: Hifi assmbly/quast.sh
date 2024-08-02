fe9193b2f39023dddd761121375a7d18508e3f91
profitamoley <lpayne4@sheffield.ac.uk>
2024-08-02 14:49:44 +0100

    Create quast.sh

---
+++ Step 2: Hifi assmbly/quast.sh
@@ -0,0 +1,11 @@
+## QUAST ##
+
+# This gives you all sorts of genome assembly info such as Genome Length, N50, GC content etc etc...
+
+# Github --> https://github.com/ablab/quast
+
+# Conda --> https://bioconda.github.io/recipes/quast/README.html
+
+# Code
+
+quast.py -o Species.quast -t 12 /path/to/Species_contigs.fasta
